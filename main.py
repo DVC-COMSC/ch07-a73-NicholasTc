e0a7b22832fd296658af10e243f76ae776b9872a
Nicholas <87298104+NicholasTc@users.noreply.github.com>
2022-10-10 14:55:45 -0700

    Update main.py

--- main.py
+++ main.py
@@ -6,5 +6,11 @@ insval = int(input('Enter the insertion value: '))
 # ******************************
 # Make your Code
 # ******************************
+prevNum = numbers[0]
+for n in numbers:
+    if insval > prevNum and insval < n:
+        numbers.insert(numbers.index(n), insval)
 
+    prevNum = n
+    
 prinnt (numbers)
