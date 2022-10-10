acc81f1fab957bbe6a4b6821451e8ed8f18e11f7
Nicholas <87298104+NicholasTc@users.noreply.github.com>
2022-10-10 15:01:46 -0700

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,12 @@ prevNum = numbers[0]
 for n in numbers:
     if insval > prevNum and insval < n:
         numbers.insert(numbers.index(n), insval)
+    elif insval < prevNum:
+        numbers.insert(0, insval)
+        break
+    elif insval > n and insval > prevNum:
+        numbers.insert(len(numbers), insval)
+        break
 
     prevNum = n
     
